3e7bb291168928c9fb43699271bf679df26e40e5
ll <1767945142@qq.com>
2019-02-25 15:40:03 +0800

    lisi第一次修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		System.out.println("zhangsan01第一次编译");
+		System.out.println("lisi第一次编译");
     }
 }
