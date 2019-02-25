9a67be68d783b4cb56cb2d752bdfd74ac5a33a5a
ll <1767945142@qq.com>
2019-02-25 17:45:11 +0800

    1

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,14 +1,14 @@
-import org.junit.Test;
+ï»¿import org.junit.Test;
 
 public class Main {
 
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-		System.out.println("zhangsan01µÚÒ»´Î±àÒë");
-		System.out.println("lisiµÚÒ»´Î±àÒë");
-		System.out.println("lisi±àÒë£¬²âÊÔ³åÍ»");
-		System.out.println("zhangsan±àÒë£¬³åÍ»²âÊÔ");
+		System.out.println("zhangsan01ç¬¬ä¸€æ¬¡ç¼–è¯‘");
+		System.out.println("lisiç¬¬ä¸€æ¬¡ç¼–è¯‘");
+		System.out.println("lisiç¼–è¯‘ï¼Œæµ‹è¯•å†²çª");
+		System.out.println("zhangsanç¼–è¯‘ï¼Œå†²çªæµ‹è¯•");
 		System.out.println("123");
 		System.out.println("456");
 		System.out.println("456");
